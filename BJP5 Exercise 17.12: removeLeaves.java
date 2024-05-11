ad8d66da416e99d3f7b496bd08fbbdea14200892
nrjiz <164118742+nrjiz@users.noreply.github.com>
2024-05-11 20:23:21 +0300

    Create BJP5 Exercise 17.12: removeLeaves.java

---
+++ BJP5 Exercise 17.12: removeLeaves.java
@@ -0,0 +1,18 @@
+/*
+* Write a method removeLeaves that removes the leaves from a tree. If your method 
+* is called on an empty tree, the method does not change the tree because there are 
+* no nodes of any kind (leaf or not). 
+*/
+
+public void removeLeaves(){
+    overallRoot=removeLeaves(overallRoot);
+}
+public IntTreeNode removeLeaves(IntTreeNode root){
+    if(root==null)return null;
+    if(root.left==null&&root.right==null)return null;    
+    else {
+    root.left=removeLeaves(root.left);
+    root.right=removeLeaves(root.right);
+    return root;
+}
+}
